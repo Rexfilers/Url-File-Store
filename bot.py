18d48f03a6f9571c1e26c63f41dc432073fbcc3d
Rexfilers <138610025+Rexfilers@users.noreply.github.com>
2023-10-03 14:03:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -74,7 +74,7 @@ async def start(bot: Client, cmd: Message):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("Updates Channel", url="https://t.me/GreyMatter_Bots")
+                        InlineKeyboardButton("Updates Channel", url="https://t.me/Movies_Provide")
                     ],
                     [
                         InlineKeyboardButton("About Bot", callback_data="aboutbot"),
@@ -82,8 +82,8 @@ async def start(bot: Client, cmd: Message):
                         InlineKeyboardButton("Close 🚪", callback_data="closeMessage")
                     ],
                     [
-                        InlineKeyboardButton("Support Group", url="https://t.me/GreyMatter_Support"),
-                        InlineKeyboardButton("YouTube Channel", url="https://bit.ly/43x8Jhe")
+                        InlineKeyboardButton("Support Group", url="https://t.me/Toffy_bot_V1_bot"),
+                        InlineKeyboardButton("YouTube Channel", url="https://t.me/Movies_Provide")
                     ]
                 ]
             )
@@ -124,7 +124,7 @@ async def main(bot: Client, message: Message):
                 return
 
         if message.from_user.id in Config.BANNED_USERS:
-            await message.reply_text("Sorry, You are banned!\n\nContact [𝙎𝙪𝙥𝙥𝙤𝙧𝙩 𝙂𝙧𝙤𝙪𝙥](https://t.me/GreyMatter_Support)",
+            await message.reply_text("Sorry, You are banned!\n\nContact [𝙎𝙪𝙥𝙥𝙤𝙧𝙩 𝙂𝙧𝙤𝙪𝙥](https://t.me/Movies_Provide2)",
                                      disable_web_page_preview=True)
             return
 
@@ -319,7 +319,7 @@ async def button(bot: Client, cmd: CallbackQuery):
                 [
                     [
                         InlineKeyboardButton("Source Codes of Bot",
-                                             url="https://greylinks.in/iNTOJzfb")
+                                             url="https://t.me/Movies_Provide")
                     ],
                     [
                         InlineKeyboardButton("Go Home", callback_data="gotohome"),
@@ -337,7 +337,7 @@ async def button(bot: Client, cmd: CallbackQuery):
                 [
                     [
                         InlineKeyboardButton("Source Codes of Bot",
-                                             url="https://greylinks.in/iNTOJzfb")
+                                             url="https://t.me/Movies_Provide")
                     ],
                     [
                         InlineKeyboardButton("About Bot", callback_data="aboutbot"),
@@ -354,7 +354,7 @@ async def button(bot: Client, cmd: CallbackQuery):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("Updates Channel", url="https://t.me/GreyMatter_Bots")
+                        InlineKeyboardButton("Updates Channel", url="https://t.me/Movies_Provide")
                     ],
                     [
                         InlineKeyboardButton("About Bot", callback_data="aboutbot"),
@@ -362,8 +362,8 @@ async def button(bot: Client, cmd: CallbackQuery):
                         InlineKeyboardButton("Close 🚪", callback_data="closeMessage")
                     ],
                     [
-                        InlineKeyboardButton("Support Group", url="https://t.me/GreyMatter_Support"),
-                        InlineKeyboardButton("YouTube Channel", url="https://bit.ly/43x8Jhe")
+                        InlineKeyboardButton("Support Group", url="https://t.me/Toffy_bot_V1_bot"),
+                        InlineKeyboardButton("YouTube Channel", url="https://t.me/Movies_Provide")
                     ]
                 ]
             )
@@ -411,8 +411,8 @@ async def button(bot: Client, cmd: CallbackQuery):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("Updates Channel", url="https://t.me/GreyMatter_Bots"),
-                        InlineKeyboardButton("Support Group", url="https://t.me/GreyMatter_Support")
+                        InlineKeyboardButton("Updates Channel", url="https://t.me/Movies_Provide"),
+                        InlineKeyboardButton("Support Group", url="https://t.me/Movies_Provide2")
                     ],
                     [
                         InlineKeyboardButton("About Bot", callback_data="aboutbot"),
